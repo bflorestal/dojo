06d2f49d70862e20acace5ef4cbcebf9768e2eab
bflorestal <yayou7710@hotmail.fr>
2024-11-11 11:26:49 +0100

    feat: implement theme provider and mode toggle components

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,24 +1,19 @@
 import type { QueryClient } from "@tanstack/react-query";
 import {
   createRootRouteWithContext,
-  Link,
   Outlet,
   ScrollRestoration,
 } from "@tanstack/react-router";
+
+import { ThemeProvider } from "~/components/theme-provider";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
 }>()({
   component: () => (
-    <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-      </div>
-      <hr />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Outlet />
       <ScrollRestoration getKey={(location) => location.pathname} />
-    </>
+    </ThemeProvider>
   ),
 });
